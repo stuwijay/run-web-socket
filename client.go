1ba20d65c43ef5472ba0ac67bf63bd4785967f6b
stuwijay <safranlimanw@gmail.com>
2024-08-22 16:09:04 +0700

    init we socket test in go

---
+++ client.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	// URL WebSocket server
+	addr := "localhost:1323"
+	wsURL := fmt.Sprintf("ws://%s/ws", addr)
+
+	// Menciptakan koneksi WebSocket
+	c, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	// Channel untuk menangani interrupt dari terminal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	done := make(chan struct{})
+
+	// Goroutine untuk menerima pesan dari server
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("Received: %s", message)
+		}
+	}()
+
+	// Mengirim pesan ke server
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Menghentikan koneksi secara bersih dengan WebSocket server
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module websocket_test
+
+go 1.22.1
+
+require github.com/gorilla/websocket v1.5.3 // indirect
